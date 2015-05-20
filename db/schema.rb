f2d49deafa93c92735320f1de6938945bf4865a8
Mai <mxngyn@gmail.com>
2015-05-19 21:10:30 -0400

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150519170045) do
+ActiveRecord::Schema.define(version: 20150519202924) do
+
+  create_table "movies", force: true do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.string   "pic_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
