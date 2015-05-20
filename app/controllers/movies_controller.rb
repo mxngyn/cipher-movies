c4547305c479e90f75a5f9d0184fa6af2be491aa
Mai <mxngyn@gmail.com>
2015-05-19 21:08:04 -0400

    Create create and new actions for Movie controller. Add movie_params

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,21 @@
 class MoviesController < ApplicationController
 
-  def show
-    @movies = Movie.search(params[:term])
-    @term = params[:term]
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      User.find(session[:user_id]).movies << @movie
+      redirect_to :controller => 'sessions', :action => 'show'
+    else
+      redirect_to :back
+      flash[:error] = "There was an error saving the movie."
+    end
   end
 
   def new
-
   end
+
+  def movie_params
+    params.require(:movie).permit(:title, :pic_url)
+  end
+
 end
