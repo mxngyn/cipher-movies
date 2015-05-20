196550fbc34bd8ab87000a2eab090f3eeebca644
Mai <mxngyn@gmail.com>
2015-05-19 21:09:05 -0400

    Add has_many movies relationship to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  has_many :movies
+
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
   validates :password, length: { minimum: 6 }, allow_nil: true
